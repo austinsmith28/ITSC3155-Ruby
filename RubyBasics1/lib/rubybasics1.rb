79e07f9627cc9a827a06a3e060f33776de916336
Austin Smith <flubahdub@gmail.com>
2019-01-29 20:30:59 +0000

    RubyBasics1 -Part 1

---
+++ RubyBasics1/lib/rubybasics1.rb
@@ -0,0 +1,18 @@
+# Lab 1
+# Part I
+def sum arr
+
+  arr.inject(0) {|total,x| total + x }
+  
+end
+
+# Part II
+def max_2_sum arr
+  # YOUR CODE HERE
+end
+
+# Part III
+def sum_to_n? arr, n
+ 
+  
+end
