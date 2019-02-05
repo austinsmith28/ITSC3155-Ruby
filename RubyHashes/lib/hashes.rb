e53af8f081263da33e9866693af53e550dccc2b8
Austin Smith <flubahdub@gmail.com>
2019-02-05 20:06:54 +0000

    RubyHashes – Part 1

---
+++ RubyHashes/lib/hashes.rb
@@ -0,0 +1,22 @@
+# RubyHashes
+# Part I
+def array_2_hash emails, contacts
+    arr = contacts.keys
+ i =0
+ while(i<emails.length)
+contacts.store(arr[i],emails[i])
+i = i+1
+ end
+ 
+ return contacts
+end
+
+# Part II
+def array2d_2_hash contact_info, contacts
+    # YOUR CODE HERE
+end
+
+# Part III
+def hash_2_array contacts
+    # YOUR CODE HERE
+end
