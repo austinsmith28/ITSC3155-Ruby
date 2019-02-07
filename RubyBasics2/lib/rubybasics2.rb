898b84955bc8c57ae190128068774edb851571ae
Austin Smith <flubahdub@gmail.com>
2019-02-07 18:36:04 +0000

    all methods complete

--- RubyBasics2/lib/rubybasics2.rb
+++ RubyBasics2/lib/rubybasics2.rb
@@ -2,15 +2,39 @@
 
 # Part I
 def hello(name)
-  # YOUR CODE HERE
+  
+  return "Hello, #{name}"
+
 end
 
 # Part II
 def starts_with_consonant? s
-  # YOUR CODE HERE
+   if /^[^aeiou\W]/i.match(s) == nil
+    return false
+   else
+    return true
+   end
+  
+
+  
+  
+  
 end
 
 # Part III
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+    if s == '0'
+        return true
+    end 
+  
+regexp = Regexp.new('^[10]*00$')
+
+ 
+  if regexp.match(s)
+      return true
+  else
+      return false
+  end
+  
+ 
 end
