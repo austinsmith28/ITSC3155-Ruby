7b1e1f7999351267bff1d6f5e0f8d50afe4e234b
Austin Smith <flubahdub@gmail.com>
2019-02-26 20:36:15 +0000

    Ruby OO Basics - part 2

--- RubyBasics3/lib/rubybasics3.rb
+++ RubyBasics3/lib/rubybasics3.rb
@@ -3,6 +3,8 @@
 class BookInStock 
     attr_accessor :isbn, :price
     def initialize(isbn, price)
+        raise ArgumentError, 'ISBN is blank' if (isbn == ("") || isbn == nil)
+        raise ArgumentError, 'price cannot be negative' if (price<=0)
         @isbn = isbn
         @price = price.to_f
     end
