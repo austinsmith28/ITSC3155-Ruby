35473c005dc2a1400a816928dcbf4c4abc3e9614
Austin Smith <flubahdub@gmail.com>
2019-03-12 20:24:15 +0000

    complete Create and Show.

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,8 @@
 class ArticlesController < ApplicationController
-    
+    def show
+        
+        @article = Article.find(params[:id])
+    end
     
     def new
         
@@ -7,6 +10,18 @@ class ArticlesController < ApplicationController
     
     def create
         
-        render plain: params[:article].inspect
+        @article = Article.new(article_params)
+        
+        @article.save
+        
+        redirect_to @article
     end
+    
+
 end
+
+
+    private
+    def article_params
+        params.require(:article).permit(:title, :text)
+    end
